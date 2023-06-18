10a26d6e7f83e3f5ab3e290ba13b5f6e57f96c48
pentaxis93 <13393192+pentaxis93@users.noreply.github.com>
2023-06-18 16:08:16 +0200

    Set supertokens dark theme

--- web/src/auth.tsx
+++ web/src/auth.tsx
@@ -38,7 +38,20 @@ isBrowser &&
             Github.init(),
             Google.init(),
           ]
-        }
+        },
+        style: `
+          [data-supertokens~=container] {
+            --palette-background: 51, 51, 51;
+            --palette-inputBackground: 41, 41, 41;
+            --palette-inputBorder: 41, 41, 41;
+            --palette-textTitle: 255, 255, 255;
+            --palette-textLabel: 255, 255, 255;
+            --palette-textPrimary: 255, 255, 255;
+            --palette-error: 173, 46, 46;
+            --palette-textInput: 169, 169, 169;
+            --palette-textLink: 169, 169, 169;
+          }
+        `
       }),
     ],
   })
